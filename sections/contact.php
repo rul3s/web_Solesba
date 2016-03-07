a668afb3315f5c259c591945fb5ec6b5bce96855
Raul Valls <valls.raul@gmail.com>
2016-03-07 18:36:16 +0100

    GUI changes form

--- sections/contact.php
+++ sections/contact.php
@@ -1,13 +1,26 @@
+
 <h2>Contacto</h2>
 <h3>Teléfono: +34 609 74 10 65</h3>
 <h3>eMail: <a href="mailto: jaume@solesba.com">jaume@solesba.com</a></h3>
 
-<h3>O si lo prefieres puedes enviarnos un mensaje a traves de la Web:</h3>
-<form action="sections/sendMail.php" method="post">
-    <input type="text" name="name" placeholder="Nombre"><br>
-    <input type="email" name="from" placeholder="eMail"><br>
-    <input type="tel" name="tel" placeholder="Teléfono"><br>
-    <input type="text" name="comments" placeholder="Mensaje"><br>
-    <!--<textarea name="msg" cols="40" rows="5" ... ></textarea><br><br>-->
+<p>
+    O si lo prefieres puedes enviarnos un mensaje con tus dudas o sugerencias
+    a traves de nuestra página Web:
+</p>
+
+<form id="mail" action="sections/sendMail.php" method="post" class="font1">
+    <div>
+        <label>Nombre:</label><br>
+        <input type="text" name="name" placeholder="Nombre"><br>
+        <label>Correo Electronico:</label><br>
+        <input type="email" name="from" placeholder="eMail"><br>
+        <label>Teléfono móvil:</label><br>
+        <input type="tel" name="tel" placeholder="Teléfono"><br>
+    </div>
+    <div>
+    <label>Mensaje:</label><br>
+        <textarea name="comments" placeholder="Mensaje"></textarea><br>
+    </div>
+    <hr>
     <input type="submit" value="Enviar">
 </form>
