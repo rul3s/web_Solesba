ffba82b57d51e9d69330edac9b14019129d8c3f2
Raul Valls <valls.raul@gmail.com>
2016-04-04 10:59:05 +0200

    Added one more gallery item

--- selectGallery.php
+++ selectGallery.php
@@ -1,20 +1,25 @@
 <figure class="optionGallery">
-
-    <img src="res/img/bar/bar.jpg" alt="barandillas">
-    <a href="index.php?section=gallery&type=bar">Barandillas</a>
+    <a href="index.php?section=gallery&type=bar">
+        <img src="res/img/bar/bar.jpg" alt="barandillas">
+        Barandillas</a>
 </figure>
 <figure class="optionGallery">
-
-    <img src="res/img/car/car.jpg" alt="caracol">
-    <a href="index.php?section=gallery&type=car">Caracol</a>
+    <a href="index.php?section=gallery&type=car">
+        <img src="res/img/car/car.jpg" alt="caracol">
+        Caracol</a>
 </figure>
 <figure class="optionGallery">
-
-    <img src="res/img/rect/rect.jpg" alt="rectas">
-    <a href="index.php?section=gallery&type=rect">Rectas</a>
+    <a href="index.php?section=gallery&type=rect">
+        <img src="res/img/rect/rect.jpg" alt="rectas">
+        Rectas</a>
 </figure>
 <figure class="optionGallery">
-
-    <img src="res/img/ext/ext.jpg" alt="exterior">
-    <a href="index.php?section=gallery&type=ext">Exterior</a>
+    <a href="index.php?section=gallery&type=ext">
+        <img src="res/img/ext/ext.jpg" alt="exterior">
+        Exterior</a>
+</figure>
+<figure class="optionGallery">
+    <a href="index.php?section=gallery&type=ext">
+        <img src="res/img/hel/hel.jpg" alt="helicoidales">
+        Helicoidal</a>
 </figure>
